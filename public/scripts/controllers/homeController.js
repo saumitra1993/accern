09489b54697e07a4186e13322f33b9af223fda3b
Saumitra Khanwalker <saumitra@Saumitras-MacBook-Air.local>
2017-01-20 20:56:33 +0530

    completed section 3, problem with bottom alignment of button to image

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -1,6 +1,25 @@
 angular.module("accern").controller("homeController", function($scope,appfactory,$q,$routeParams){
     
-    
+    var acc = document.getElementsByClassName("accordion");
+    console.log(acc);
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function(){
+            console.log("Here")
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight){
+              panel.style.maxHeight = null;
+            } else {
+              panel.style.maxHeight = panel.scrollHeight + 'px';
+            } 
+        }
+    }
     
     
     
