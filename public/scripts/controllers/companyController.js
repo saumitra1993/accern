b7891e9d53d68248e72ca5294a470ef22896a570
Saumitra Khanwalker <saumitra@Saumitras-MacBook-Air.local>
2017-01-23 07:03:44 +0530

    sass stylesheets reorganised, resources containers bug fixes

--- public/scripts/controllers/companyController.js
+++ public/scripts/controllers/companyController.js
@@ -1,10 +1,9 @@
 angular.module("accern").controller("companyController", function($scope,appfactory,$q,$routeParams){
     
-     $scope.partners1 = ["IBM Investment Solutions","Bloomberg App Store","Interactive Brokers Marketplace","Quantopian Data Store"];
-    $scope.partners2 = ["Quandl Store","QuantConnect Store","StockTwits Ad Partner","Benzinga Distribution Partner"];
+     $scope.partner_rows = [["IBM Investment Solutions","Bloomberg App Store","Interactive Brokers Marketplace","Quantopian Data Store"], ["Quandl Store","QuantConnect Store","StockTwits Ad Partner","Benzinga Distribution Partner"]];
     
     $scope.getImageSrc = function(partner){
-        console.log(partner);
+        
         path_arr =  partner.split(' ');
         path_arr.push("logo");
         path = "images/" + path_arr.join('-').toLowerCase()+".png";
