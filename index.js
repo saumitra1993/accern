2c21337db5de16a9321edd514e4ce07b58274940
saumitra1993 <theapogee2011@gmail.com>
2017-01-23 11:53:00 +0530

    Correct port settings for heroku deploy

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
   res.render('index');
 })
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 });
