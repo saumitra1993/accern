2cfae56357ebd4d6043f6b11f99543f5336da030
saumitra1993 <theapogee2011@gmail.com>
2017-01-25 17:24:06 +0530

    corrected accordion padding cross-browser problems, replaced js in controllers with directive

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -15,9 +15,19 @@ angular.module("accern").controller("homeController", function($scope,appfactory
 
        }; 
     }
+
+    $scope.features1 = [{head:"Use Trading Analytics", description:"We derive analytics such as sentiment, impact, news exposure, news reliability rankings, and much more from each article."},
+                        {head:"Trade On Early News", description:"Our massive coverage of 300 million websites allows us to find and deliver early stories on U.S. public equities to our customers."},
+                        {head:"Build Custom Alerts",description:"Our app allows you to build custom news alerts geared specifically towards your trading strategy. If requested, we can even help you build them."}
+                        ];
+
+    $scope.features2 = [{head:"Flexible Low-Latency API Feed",description:"Our push-based low-latency JSON API feed can aggregate data based on specific time frames or filter data based on companies."},
+                        {head:"API Data Fields",description:"Our API data feed includes article metadata such as URLs, events, companies, and derived analytics such as sentiment."},
+                        {head:"4 Years Of History",description:"We have over four years of historical data, which includes article headlines and URLs. Historical data are provided upon request."}
+                        ];
     
         
-    var acc = document.getElementsByClassName("accordion");
+   /* var acc = document.getElementsByClassName("accordion");
     console.log(acc);
     var i;
 
@@ -25,10 +35,10 @@ angular.module("accern").controller("homeController", function($scope,appfactory
         acc[i].onclick = function(){
             console.log("Here")
             /* Toggle between adding and removing the "active" class,
-            to highlight the button that controls the panel */
+            to highlight the button that controls the panel 
             this.classList.toggle("active");
 
-            /* Toggle between hiding and showing the active panel */
+            /* Toggle between hiding and showing the active panel 
             var panel = this.nextElementSibling;
             if (panel.style.maxHeight){
               panel.style.maxHeight = null;
@@ -53,7 +63,38 @@ angular.module("accern").controller("homeController", function($scope,appfactory
         
         
     }
-    
+    */
     
     
 });
+
+angular.module("accern").directive('expandAccordionPanel', [function() {
+
+  function link(scope, element, attrs) {
+    var open,
+        timeoutId;
+
+    function updateVisibility() {
+        if(open){
+            var height = element[0].scrollHeight;
+            element[0].style.maxHeight = height + "px";    
+        }
+        else{
+            element[0].style.maxHeight = 0;       
+        }
+        
+    }
+
+    scope.$watch(attrs.expandAccordionPanel, function(value) {
+      open = value;
+      updateVisibility();
+    });
+
+    
+  }
+
+  return {
+    link: link
+  };
+
+}]);
